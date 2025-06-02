a5ea589c961d88eea112e81149bc3e4ace794476
errishabhsrivastava <ersrivastavarishabh@gmail.com>
2025-06-02 10:25:53 +0530

    v0.0.5

--- middlewares/navbarMiddleware.js
+++ middlewares/navbarMiddleware.js
@@ -1,22 +1,18 @@
 const jwt = require('jsonwebtoken');
 const axios = require('axios'); // for internal API call
 
-const authMiddleware = async (req, res, next) => {
+const navbarMiddleware = async (req, res, next) => {
   const token = req.cookies.gwsToken;
 
   if (!token) {
-
     console.log('No token provided'); 
     return res.redirect('/login'); // Redirect to login if no token
-    
   }
-
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     const userId = decoded.id;
-    console.log('Decoded User ID:', userId);
     // Call your own internal API to get full user details
-    const apiURL = `https://auth.gws365.in/getuserById/${userId}`;
+    const apiURL = `https://auth.genexwebservices.com/getuserById/${userId}`;
     const response = await axios.get(apiURL); // You can also include auth headers if needed
 
     if (response.data && response.data.user) {
